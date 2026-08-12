85027b6cc7b7995e96b0b630617f0d6a2e498f53
ernoadewin <kmo2610@naver.com>
2026-08-12 11:13:00 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('year').textContent=new Date().getFullYear();
-const menu=document.querySelector('.menu'),nav=document.querySelector('.header nav');
-menu.addEventListener('click',()=>nav.classList.toggle('open'));
-nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')));
+const b=document.querySelector('.menu'),n=document.querySelector('nav');b.addEventListener('click',()=>n.classList.toggle('open'));n.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>n.classList.remove('open')));
